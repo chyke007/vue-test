20008a3bb79a66caf19266a56cb39603ff8475d3
Nwachukwu Chibuike <nwachukwuchibuike2012@gmail.com>
2021-01-14 16:26:44 +0100

    Learns more about mount and shallowMount

---
+++ tests/unit/SubmitButton.spec.js
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import SubmitButton from '@/components/SubmitButton.vue'
+
+
+const msg = "submit"
+const factory = (props) => {
+    return mount(SubmitButton, {
+       props:{
+        msg,
+        ...props
+       }
+    })
+}
+
+describe('SubmitButton.vue', () => {
+    it('displays a non authorized message', () => {
+        const wrapper  = factory()
+        expect(wrapper.find("span").text()).toBe("Not Authorized")
+        expect(wrapper.find("button").text()).toBe("submit")
+    })
+})
+
+describe('SubmitButton.vue', () => {
+    it('displays a admin privileges message', () => {
+        const wrapper  = factory({isAdmin: true})
+        expect(wrapper.find("span").text()).toBe("Admin Privileges")
+        expect(wrapper.find("button").text()).toBe("submit")
+    })
+})
